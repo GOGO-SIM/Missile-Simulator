3f5b2b44195a38080e9566601f52a7558de56a00
jeongyunnim <jeongyun7521@gmail.com>
2025-05-22 14:09:01 +0900

    feat: 미사일 폭발 시 움직임 정지 기능

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,11 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MissileExplosion : MonoBehaviour
 {
     public GameObject explosionEffect;
-    public float explosionRadius = 3f;
+    public float explosionRadius = 20f;
     public LayerMask targetLayer;
-
+    public GameObject imuObject;
+    public GameObject seekerObject;
     private Rigidbody rb;
 
     void Start()
@@ -24,12 +26,26 @@ public class MissileExplosion : MonoBehaviour
                 if (script != this) // 자기 자신 제외
                     script.enabled = false;
             }
+            // 타겟 멈춤
+            TargetMover targetMover = other.GetComponent<TargetMover>();
+            if (targetMover != null)
+            {
+                targetMover.enabled = false;
+            }
 
             // 궤적 멈춤
             MissileTrail trail = GetComponent<MissileTrail>();
             if (trail != null)
                 trail.isFrozen = true;
             // 폭발 효과
+            if (imuObject != null)
+            {
+                Destroy(imuObject);
+            }
+            if (seekerObject != null)
+            {
+                Destroy(seekerObject);
+            }
             Explode();
         }
     }
@@ -39,7 +55,9 @@ public class MissileExplosion : MonoBehaviour
     {
         if (explosionEffect != null)
         {
-            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Vector3 explosionPosition = transform.position + transform.forward * 10.0f;
+ 
+            Instantiate(explosionEffect, explosionPosition, Quaternion.identity);
         }
     }
 }
